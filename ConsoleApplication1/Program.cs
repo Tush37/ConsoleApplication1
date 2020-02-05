2654d1ad2b491c088008f37576e6d035a479e12a
Tushar Pawar <pawar.tushar95@gmail.com>
2020-02-06 01:16:11 +0530

    v1 added

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hellol in c#");
+            Console.WriteLine("Hellol in c# V1");
             Console.ReadLine();
         }
     }
